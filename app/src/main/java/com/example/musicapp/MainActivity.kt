e8ac9e7de317c436ddb6ae510ccce284c6a03344
Rajiv_Nayan <rajivnayan100@gmail.com>
2023-04-10 21:37:33 +0530

    Uploading new

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -3,22 +3,22 @@ package com.example.musicapp
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val playButton = findViewById<ImageButton>(R.id.play_btn)
-        val mediaplayer : MediaPlayer = MediaPlayer.create(this, R.raw.music)
+        var playButton = findViewById<ImageButton>(R.id.play_btn)
+        mediaPlayer = MediaPlayer.create(this, R.raw.music)
         playButton.setOnClickListener {
-                if (!mediaplayer.isPlaying){
-                    mediaplayer.start()
+                if (!mediaPlayer.isPlaying){
+                    mediaPlayer.start()
                     playButton.setImageResource(R.drawable.baseline_pause_24)
 
                 }else{
-                    mediaplayer.pause()
+                    mediaPlayer.pause()
                     playButton.setImageResource(R.drawable.baseline_play_arrow_24)
                 }
 
