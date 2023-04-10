ad0a85f9b5ac65c74768ec9cc4d234766a7a5b1a
Rajiv_Nayan <rajivnayan100@gmail.com>
2023-04-10 23:10:02 +0530

    Uploading new

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -4,13 +4,15 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.SeekBar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var playButton = findViewById<ImageButton>(R.id.play_btn)
+
+        val playButton = findViewById<ImageButton>(R.id.play_btn)
         mediaPlayer = MediaPlayer.create(this, R.raw.music)
         playButton.setOnClickListener {
                 if (!mediaPlayer.isPlaying){
@@ -25,5 +27,29 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        val seekbar: Nothing? = null
+        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, pos: Int, changed: Boolean) {
+                if (changed){
+                    mediaPlayer.seekTo(pos)
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+
+
     }
 }
+
+private fun Nothing?.setOnSeekBarChangeListener(onSeekBarChangeListener: SeekBar.OnSeekBarChangeListener) {
+
+}
+
+
